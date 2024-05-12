8a118a5f0ef9c76e8db58cf3c7d59aa8bad99917
Theo Vitor <theo.vitor703@gmail.com>
2024-05-12 16:38:32 -0300

    feat: add initial category component

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: { remotePatterns: [{ hostname: "utfs.io" }] },
+};
 
 export default nextConfig;
